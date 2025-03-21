75ab5b852cb905446eda3d6aa721b0dfa0ddab3f
artemykozhevnik <artemykozhevnik@gmail.com>
2025-03-21 13:10:11 +0300

    Rectangle incorrect. Works only while holding breakpoints.

--- example/main.cpp
+++ example/main.cpp
@@ -7,6 +7,57 @@
 #include "iostream"
 #include <chrono>
 
+
+void turn_right(mavlink_message_t message, Generic_Port *port){
+    // turn right
+    mavlink_msg_set_position_target_local_ned_pack(
+        255,                                // Sender system ID
+        MAV_COMP_ID_ONBOARD_COMPUTER,        // Sender component ID
+        &message,                                // MAVLink message to pack into
+        0,                                   // Timestamp (not used)
+        0,                                   // Target system ID
+        0,                                   // Target component ID
+        MAV_FRAME_BODY_OFFSET_NED,                 // Coordinate frame
+        2503,                                   // Type mask (velocity control)
+        0, 0, 0,                             // x, y, z position (not used)
+        0, 0, 0,                             // vx, vy, vz velocity (1 m/s North)
+        0, 0, 0,                             // afx, afy, afz acceleration (not used)
+        1.5708,                                   // Yaw angle 45d
+        0                                    // Yaw rate (not used)
+    );
+    int len = port->write_message(message_set_pos);
+    if (len <= 0) {
+        std::cerr << "WARNING (R): could not send mavlink_msg_set_position_target_local_ned_pack" << std::endl;
+    } else {
+        std::cout << "Requested mavlink_msg_set_position_target_local_ned_pack message" << std::endl;
+    }
+}
+
+void go_forward_10_m(mavlink_message_t message, Generic_Port *port){
+    // go forward
+    mavlink_msg_set_position_target_local_ned_pack(
+        255,                                // Sender system ID
+        MAV_COMP_ID_ONBOARD_COMPUTER,        // Sender component ID
+        &message,                                // MAVLink message to pack into
+        0,                                   // Timestamp (not used)
+        0,                                   // Target system ID
+        0,                                   // Target component ID
+        MAV_FRAME_BODY_OFFSET_NED,                 // Coordinate frame
+        3576,                                   // Type mask (velocity control)
+        100, 0, 0,                             // x, y, z position (not used)
+        0, 0, 0,                             // vx, vy, vz velocity (1 m/s North)
+        0, 0, 0,                             // afx, afy, afz acceleration (not used)
+        0,                                   // Yaw angle (not used)
+        0                                    // Yaw rate (not used)
+    );
+    int len = port->write_message(message_set_pos);
+    if (len <= 0) {
+        std::cerr << "WARNING (F): could not send mavlink_msg_set_position_target_local_ned_pack" << std::endl;
+    } else {
+        std::cout << "Requested mavlink_msg_set_position_target_local_ned_pack message" << std::endl;
+    }
+}
+
 int main(int argc, char **argv)
 {
     cxxopts::Options options("mav_timesync", "mavlink time syncronisation");
@@ -53,88 +104,97 @@ int main(int argc, char **argv)
 
     port->start();
     uint64_t timesync_last = 0;
-    bool success;
+    bool success;   // response result
+
+    bool allow_right = 1;  // send command
+    bool is_turning_right = 0;  // info flag
+    bool is_going_forward = 0;  // info flag
+    bool allow_forward = 0;  // send command
+
+    bool need_mission_state = true;
+    mavlink_mission_current_t mission_current;
     // mav_timesync timesync;
+
+
+    
+
     while (true)
     {
         mavlink_message_t message;
         success = port->read_message(message);
+        
+        
+        //MAV_CMD_REQUEST_MESSAGE
         if (success)
-        {
-            uint64_t _now = std::chrono::duration_cast<std::chrono::microseconds>(
-                                std::chrono::system_clock::now().time_since_epoch())
-                                .count();
-            switch (message.msgid)
-            {
-            case MAVLINK_MSG_ID_TIMESYNC:
-            {
-                mavlink_timesync_t timesync_msg;
-                mavlink_msg_timesync_decode(&message, &timesync_msg);
-                // if (timesync.f_time_sync(timesync_msg))
-                // {
+        {  
 
-                //     mavlink_message_t msg;
-                //     mavlink_msg_timesync_encode(255, MAV_COMP_ID_ONBOARD_COMPUTER, &msg,
-                //                                 &timesync_msg);
-                //     int len = port->write_message(msg);
-                //     std::cout << "SEND_TIMESYNC: " << timesync_msg.tc1 << '\t'
-                //               << timesync_msg.ts1 << std::endl;
-                //     // std::cout << "dt: " << timesync_msg.tc1 - timesync_msg.ts1 << " ns"
-                //     //   << std::endl;
-                //     // check the write
-                //     if (len <= 0)
-                //         fprintf(stderr, "WARNING: could not send TIMESYNC \n");
-                // }
+            if (need_mission_state){
 
-                mavlink_system_time_t system_time;
-                system_time.time_unix_usec =
-                    std::chrono::duration_cast<std::chrono::microseconds>(
-                        std::chrono::system_clock::now().time_since_epoch())
-                        .count();
-                system_time.time_boot_ms = 0;
-                mavlink_message_t msg1;
-                mavlink_msg_system_time_encode(255, MAV_COMP_ID_ONBOARD_COMPUTER, &msg1,
-                                               &system_time);
-                std::cout << "SEND SYST_TIME: " << system_time.time_unix_usec
-                          << std::endl;
-                int len = port->write_message(msg1);
-                std::time_t now = std::time(nullptr);
-                std::cout << "Current time: " << std::asctime(std::localtime(&now))
-                          << std::endl;
-                if (len <= 0)
-                    fprintf(stderr, "WARNING: could not send SYSTEM_TIME \n");
-
-                break;
+                mavlink_command_long_t cmd = {};
+                cmd.target_system = 1; // Target system ID
+                cmd.target_component = 1; // Target component ID
+                cmd.command = MAV_CMD_REQUEST_MESSAGE;
+                cmd.param1 = MAVLINK_MSG_ID_MISSION_CURRENT; // Request MISSION_CURRENT message
+                cmd.param2 = 0; // Unused
+                cmd.param3 = 0; // Unused
+                cmd.param4 = 0; // Unused
+                cmd.param5 = 0; // Unused
+                cmd.param6 = 0; // Unused
+                cmd.param7 = 0; // Unused
+            
+                mavlink_message_t msg;
+                mavlink_msg_command_long_encode(255, MAV_COMP_ID_ONBOARD_COMPUTER, &msg, &cmd);
+            
+                int len = port->write_message(msg);
+                if (len <= 0) {
+                    std::cerr << "WARNING: could not send MAV_CMD_REQUEST_MESSAGE" << std::endl;
+                } else {
+                    std::cout << "Requested MISSION_CURRENT message" << std::endl;
+                }
+            
+                need_mission_state = false;
             }
-            case MAVLINK_MSG_ID_SYSTEM_TIME:
-            {
-                mavlink_system_time_t system_time_msg;
-                mavlink_msg_system_time_decode(&message, &system_time_msg);
+            else {
+                mavlink_mission_current_t mission_current;
+                mavlink_msg_mission_current_decode(&message, &mission_current);
+                need_mission_state = true;
+                
+                switch (mission_current.mission_state)
+                {
+                    case MISSION_STATE_NO_MISSION:
+                        mavlink_message_t message_set_pos;
+                        if (allow_right){
+                            turn_right(message_set_pos);
+                            allow_right = false;
+                            is_turning_right = true;
+                            
+                        }
+                        else if (allow_forward) {
+                            go_forward_10_m(message_set_pos);
+                            allow_forward = false;
+                            is_going_forward = true;
+                            
+                        }
+                        break;
+                    
+                    case MISSION_STATE_COMPLETE:
+                        
+                        if (is_turning_right) {
+                            std::cout<<"COMPLETE RIGHT"<<std::endl;
+                        
+                            allow_forward = true;
+                            is_turning_right = false;
+                        }
+                        else if (is_going_forward) {
+                            std::cout<<"COMPLETE FORWARD"<<std::endl;
+                        
+                            allow_right = true;
+                            is_going_forward = false;
+                        }
 
-                std::cout << "ARDUPILOT SYST_TIME: " << system_time_msg.time_unix_usec
-                          << std::endl;
-
-                ;
-                break;
-            }
-            }
-
-            if (_now - timesync_last > ((float)1. / timesync_hz) * 1e6)
-            {
-
-                mavlink_system_time_t system_time;
-                system_time.time_unix_usec =
-                    std::chrono::duration_cast<std::chrono::microseconds>(
-                        std::chrono::system_clock::now().time_since_epoch())
-                        .count();
-                system_time.time_boot_ms = 0;
-                mavlink_message_t msg1;
-                mavlink_msg_system_time_encode(255, MAV_COMP_ID_ONBOARD_COMPUTER, &msg1,
-                                               &system_time);
-                std::cout << "SEND SYST_TIME: " << system_time.time_unix_usec
-                          <<'\t'<< _now - timesync_last <<'\t'<< ((float)1. / timesync_hz) * 1e6 << std::endl;
-                int len = port->write_message(msg1);
-                timesync_last = _now;
+                        break;
+                    
+                }
             }
         }
     }
